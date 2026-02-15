0ffeafce2fe838c2e1d4c4fc417a4ad5d2a4add8
Rashmi Ranjan Parija <126317142+rashmiofficial45@users.noreply.github.com>
2026-02-15 17:43:04 +0530

    created the whole project with CI/CD support with testing capabilities

---
+++ src/db.ts
@@ -0,0 +1,17 @@
+import { PrismaClient } from "@prisma/client";
+import { Pool } from "pg";
+import { PrismaPg } from "@prisma/adapter-pg";
+
+// 1. Create a PostgreSQL connection pool
+const connectionString = process.env.DATABASE_URL;
+const pool = new Pool({ connectionString });
+
+// 2. Create the Prisma adapter
+const adapter = new PrismaPg(pool);
+
+// 3. Pass the adapter to PrismaClient
+export const prismaClient = new PrismaClient({ adapter });
+
+// console.log(Object.keys(prismaClient.sum)) // the keys of the prisma client object
+// What if we could mock out all these keys in a single function call?
+// we want to achieve that by using one library @vitest-mock-extended
---
+++ src/tests/index.test.ts
@@ -0,0 +1,55 @@
+import { describe, expect, it, vi } from "vitest";
+import { userfn } from "../utils";
+/**
+ * ─────────────────────────────────────────────
+ *  OPTIONAL: Mock Prisma (recommended for unit tests)
+ * ─────────────────────────────────────────────
+ * Now it is working perfectly.
+ */
+// vi.mock("../db", () => ({
+//   prismaClient: {
+//     sum: {
+//       create: vi.fn()
+//     }
+//   }
+// }));
+
+//mocking the db module deeply (mockDeep)
+vi.mock("../db")
+vi.mock("../utils") //IMP: this line activates the mock. commenting this line will deactivate the mock even if the the mock file exists...
+
+import request from "supertest";
+import { app } from "../index";
+
+describe("POST /sum", () => {
+  describe("When inputs are valid", () => {
+    it("should return 200 and correct sum", async () => {
+      const res = await request(app)
+        .post("/sum")
+        .send({ a: 1, b: 2 }); // Using 1 and 2 to be explicit
+      console.log(userfn())
+      expect(res.statusCode).toBe(200);
+      expect(res.body.answer).toBe(3);
+    });
+  });
+
+  describe("When inputs are invalid", () => {
+    it("should return 411 for wrong input types", async () => {
+      const res = await request(app)
+        .post("/sum")
+        .send({
+          a: ["asdasdasd"], // invalid type
+          b: 2,
+        });
+
+      expect(res.statusCode).toBe(411);
+      expect(res.body.message).toBe("Incorrect inputs");
+    });
+
+    it("should return 411 for empty body", async () => {
+      const res = await request(app).post("/sum").send({});
+
+      expect(res.statusCode).toBe(411);
+    });
+  });
+});
